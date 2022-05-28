2edd5462da7d5a0599ff3e2d4ca8fb795656449b
Youmin Qi <qiyoumin0801@163.com>
2022-05-28 08:32:35 +0800

    init

---
+++ bash/.bashrc
@@ -0,0 +1,17 @@
+
+# git branch display
+function git_branch {
+    branch="`git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //"`"
+    if [ "${branch}" != "" ];then
+        if [ "${branch}" = "(no branch)" ];then
+            branch="(`git rev-parse --short HEAD`...)"
+        fi
+        echo " ($branch)"
+    fi
+}
+export PS1='\u@\h \[\033[01;36m\]\W\[\033[01;32m\]$(git_branch)\[\033[00m\] \n\$ '
+
+
+alias ls='ls --color=auto'
+alias ll='ls -lrh'
+alias lla='ll -a'
---
+++ setup_all.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# PROGRAMS=(bash git tmux vim zsh)
+PROGRAMS=(bash git vim)
+OLD_DOTFILES="dotfile_bk_$(date -u +"%Y%m%d%H%M%S")"
+mkdir $OLD_DOTFILES
+
+function backup_if_exists() {
+    if [ -f $1 ];
+    then
+      mv $1 $OLD_DOTFILES
+    fi
+    if [ -d $1 ];
+    then
+      mv $1 $OLD_DOTFILES
+    fi
+}
+
+# Clean common conflicts
+backup_if_exists ~/.bashrc
+# backup_if_exists ~/.zshrc
+backup_if_exists ~/.gitconfig
+# backup_if_exists ~/.tmux.conf
+backup_if_exists ~/.vimrc
+
+for program in ${PROGRAMS[@]}; do
+  stow -v --target=$HOME $program
+  echo "Configuring $program"
+done
+
+echo "Done!"
