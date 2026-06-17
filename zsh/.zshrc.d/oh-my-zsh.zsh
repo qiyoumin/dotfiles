6a62f29e2c4036483fc6ab27462877eeb83de7ef
qiyoumin <qiyoumin0801@163.com>
2026-06-17 18:57:32 +0800

    docs: restore Oh My Zsh template comments
    - Restore the explanatory Oh My Zsh template comments around theme and plugin settings.
    - Keep the existing guarded Oh My Zsh loading behavior unchanged.
    - Preserve dynamic optional plugin detection while documenting the original plugin list.
    - Keep user configuration examples available inside the Oh My Zsh fragment.

--- zsh/.zshrc.d/oh-my-zsh.zsh
+++ zsh/.zshrc.d/oh-my-zsh.zsh
@@ -1,20 +1,85 @@
-# Oh My Zsh setup, enabled only when it is installed on this machine.
+## OMZ settings
 
+# Path to your Oh My Zsh installation.
 export ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 if [ ! -r "$ZSH/oh-my-zsh.sh" ]; then
   return
 fi
 
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time Oh My Zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+# ZSH_THEME="random"
 ZSH_THEME="${ZSH_THEME:-apple}"
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in $ZSH/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "apple" "bira" )
+
+# Uncomment the following line to use case-sensitive completion.
 CASE_SENSITIVE="${CASE_SENSITIVE:-true}"
+
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line to change how often to auto-update (in days).
+# zstyle ':omz:update' frequency 13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
 DISABLE_AUTO_TITLE="${DISABLE_AUTO_TITLE:-true}"
 
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
 _omz_plugins=(
   git
   z
 )
 
+# plugin: zsh-completions
 _zsh_custom="${ZSH_CUSTOM:-$ZSH/custom}"
 _zsh_completion_dir="$_zsh_custom/plugins/zsh-completions/src"
 if [ -d "$_zsh_completion_dir" ]; then
@@ -29,9 +94,46 @@ for _plugin in zsh-autosuggestions zsh-syntax-highlighting; do
 done
 unset _plugin
 
+# plugins=(git
+#         z
+#         # zsh-vi-mode
+#         zsh-autosuggestions
+#         zsh-syntax-highlighting
+#         zsh-completions)
 plugins=("${_omz_plugins[@]}")
 unset _omz_plugins _zsh_completion_dir _zsh_custom
 
+# adapt to Iterm2's theme: Solarized Dark
+# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#93a1a1'
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="${ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE:-fg=#586e75}"
 
 source "$ZSH/oh-my-zsh.sh"
+
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='nvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch $(uname -m)"
+
+# Set personal aliases, overriding those provided by Oh My Zsh libs,
+# plugins, and themes. Aliases can be placed here, though Oh My Zsh
+# users are encouraged to define aliases within a top-level file in
+# the $ZSH_CUSTOM folder, with .zsh extension. Examples:
+# - $ZSH_CUSTOM/aliases.zsh
+# - $ZSH_CUSTOM/macos.zsh
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
